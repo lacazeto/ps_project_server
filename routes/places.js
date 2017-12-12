13dd546d977749c31c7da048cef732cab26ecad5
lacazeto <lucas.cazeto@gmail.com>
2017-12-12 13:53:17 +0100

    added place register route

--- routes/places.js
+++ routes/places.js
@@ -22,9 +22,25 @@ router.get("/:id", function(req, res, next) {
   });
 });
 
-router.post("/place", function(req, res, next) {
-  const name = req.body.name;
-  // todo
+router.post("/", function(req, res, next) {
+  const owner = req.body.owner;
+  const description = req.body.description;
+  const price = req.body.price;
+  const isEnabled = req.body.isEnabled;
+  const typeAccepted = req.body.type;
+  const newPlace = Place({
+    owner: owner,
+    description: description,
+    price: price,
+    isEnabled: isEnabled,
+    type_accepted: typeAccepted
+  });
+  newPlace.save((err) => {
+    if (err) {
+      next(err);
+    }
+    res.status(200).json({});
+  });
 });
 
 module.exports = router;
